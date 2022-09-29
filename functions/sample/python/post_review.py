402bb71353a2c2a52b177b709caac3e8407067f7
dkbakke <dbakkepublic@gmail.com>
2022-09-29 19:11:56 -0400

    post_review edits

--- functions/sample/python/post_review.py
+++ functions/sample/python/post_review.py
@@ -1,5 +1,4 @@
 # post_review - IBM Cloud Function 
-
 from ibmcloudant.cloudant_v1 import Document, CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core import ApiException
@@ -11,34 +10,14 @@ def main(dict):
         authenticator = IAMAuthenticator(dict["IAM_API_KEY"])
         service = CloudantV1(authenticator=authenticator)
         service.set_service_url(dict["COUCH_URL"])
-        if ( "review" not in dict ):
-            review = { "review": 
-                {   "id": "1114",
-                    "name": "Upkar Lidder",
-                    "dealership": 15,
-                    "review": "Great service!",
-                    "purchase": False,
-                    "purchase_date": "02/16/2021",
-                    "car_make": "Audi",
-                    "car_model": "Car",
-                    "car_year": 2021
-                } 
-            } 
-        else: 
-            review = dict
-
-        review_doc = Document(
-            id = review["review"]["id"],
-            name = review["review"]["name"],
-            dealership = review["review"]["dealership"],
-            review =  review["review"]["review"],
-            purchase = review["review"]["purchase"],
-            purchase_date = review["review"]["purchase_date"],
-            car_make = review["review"]["car_make"],
-            car_model = review["review"]["car_model"],
-            car_year = review["review"]["car_year"]
-        )
-        response = service.post_document(db=database_name, document=review_doc).get_result()
+        if ( "review" in dict ): 
+            review = dict["review"]
+        else:
+            return { 
+                "statusCode" : 500,
+                 "error" : "No review provided"
+                }
+        response = service.post_document(db=database_name, document=review).get_result()
         print(response)     
     except ApiException as ae:
         print("Method failed")
@@ -46,9 +25,18 @@ def main(dict):
         print(" - error message: " + ae.message)
         if ("reason" in ae.http_response.json()):
             print(" - reason: " + ae.http_response.json()["reason"])
-        return {"apiexception": ae.message}    
+        return {
+            "statusCode" : 500,
+            "apiexception": ae.message
+            }    
     except (requests.exceptions.RequestException, ConnectionResetError) as err:
         print("connection error")
-        return {"error": err}
-
-    return { "response" : response }
+        return {
+            "statusCode" : 500,
+            "error": err
+            }
+    if ( response["ok"] ):
+        return { 
+                "statusCode" : 200, 
+                "response" : response
+         }
